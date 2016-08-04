b666c8f2b5226e4b8818e0b4f7776531cef5cdb8
ssoso27 <sohi8290@naver.com>
2016-08-04 23:04:59 +0900

    맵생성_움직이는장애물

---
+++ Practice/Assets/CameraWork.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class CameraWork : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(player.transform.position.x + 4.61f, transform.position.y, transform.position.z);
+	}
+}
---
+++ Practice/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    float delta = 0.07f;
+
+   // GameManager gmComponent = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+	// Use this for initialization
+	void Start () {
+        //gmComponent.MakeMap();
+        GameObject.Find("GameManager").GetComponent<GameManager>().MakeMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerX = transform.position.x; // Player의 X좌표
+    //  byte jumpNum = 0; // 점프 두 번으로 제한
+                                              
+     //   playerX += Input.GetAxis("Horizontal") * 0.1f; // Player 좌우이동
+        playerX += delta; // Player 자동이동
+        transform.position = new Vector3(playerX, transform.position.y, transform.position.z);
+        
+
+        // 점프
+        if (Input.GetKeyDown(KeyCode.Space)) // Space 입력
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+        }
+
+	}
+}
