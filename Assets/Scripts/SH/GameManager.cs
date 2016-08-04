5d678246d23d63ce337f9992269fb38d1119c05f
Lming1 <spear131121@gmail.com>
2016-08-04 23:28:18 +0900

    폴더정리

--- Assets/Scripts/SH/GameManager.cs
+++ Assets/Scripts/SH/GameManager.cs
@@ -1,30 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
-namespace SH
-{
-    public class GameManager : MonoBehaviour
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject mapA;
+
+    public void MakeMap() // 맵 생성 메소드
     {
-
-
-
-
-        // Use this for initialization
-        void Start()
+        // MapA 생성
+        for (int i = 0; (20 * i) < 700; i++)
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-        public void RestartGame()
-        {
-
-            SceneManager.LoadScene("test");
-
+            Vector3 mapAPosition = new Vector3(20*i, 2.5f, 0);
+            Debug.Log("Map A 생성 " + i);
+            Instantiate(mapA, mapAPosition, Quaternion.identity);
         }
     }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
