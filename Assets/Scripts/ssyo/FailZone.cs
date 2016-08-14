aa61d481c3cfdf0f9de0d301bb9994946bc15c08
tmdrnjs54 <tmdrnjs54@naver.com>
2016-08-14 21:45:23 +0900

    FailZone

---
+++ Assets/Scripts/ssyo/FailZone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+namespace ssyo
+{
+
+    public class FailZone : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.name == "Player")
+            {
+                SceneManager.LoadScene("Practice one");
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+    }
+}
