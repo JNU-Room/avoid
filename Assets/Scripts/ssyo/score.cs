c213935015da7772f2fb41288c9b1cf64b7e88bd
tmdrnjs54 <tmdrnjs54@naver.com>
2016-08-28 16:22:53 +0900

    score

---
+++ Assets/Scripts/ssyo/score.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Text score_te;
+    public float score_ti;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        score_ti++;
+        
+        score_te.text = score_ti + "점";
+
+
+
+    }
+}
