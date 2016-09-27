a149822d7a79616417eada47d02f59c7fc727cbf
tmdrnjs54 <tmdrnjs54@naver.com>
2016-09-27 17:42:45 +0900

    Help and Help2

---
+++ Assets/Scripts/ssyo/After.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class After : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void click()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
+
---
+++ Assets/Scripts/ssyo/Back.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Back : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void click()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
