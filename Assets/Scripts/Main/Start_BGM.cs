a655ae7406a86910a2b2b40bf40ed32424c9ad81
Lming1 <spear131121@gmail.com>
2016-10-08 20:00:16 +0900

    피격음 재구현 및 Start Scene BGM 설정

---
+++ Assets/Scripts/Main/Start_BGM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_BGM : MonoBehaviour {
+    public AudioClip startBGM;
+    public AudioClip dmgChkBGM;
+    public AudioSource _audio;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void StartGame()
+    {
+        _audio.clip = startBGM;
+        _audio.Play();
+        SceneManager.LoadScene("Practice one");
+    }
+}
