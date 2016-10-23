3b8046f5e0f0f271682c2aa2645b532ca320a695
w1193 <w1193@naver.com>
2016-10-23 22:45:42 +0900

    Add files via upload

---
+++ Assets/Scripts/SB/coin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
